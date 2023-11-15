5d2960b0e6366683d585d099d624efe641a1bd34
pclausen94 <pclausen94@gmail.com>
2023-11-15 17:45:55 +0100

    db setup and testing
    Data now shows live as intended

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
-function App() {
-  return (
-    <div className="App">
-      <h2>todo App</h2>
-    </div>
-  );
+import { Component } from "react";
+import { app } from "./firebase";
+import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: [],
+    };
+  }
+
+  async refreshNotes() {
+    var notesList = [];
+    const db = getFirestore(app);
+    const notesCol = collection(db, "notes");
+    const notesSnapshot = await getDocs(notesCol);
+
+    notesSnapshot.forEach((doc) => {
+      let note = doc.data();
+      note.id = doc.id;
+      notesList.push(note);
+    });
+
+    this.setState({ notes: notesList });
+  }
+
+  componentDidMount() {
+    this.refreshNotes();
+  }
+
+  render() {
+    const { notes } = this.state;
+    console.log(notes);
+    return (
+      <div className="App">
+        <h2>todo App</h2>
+        {notes.map((note) => (
+          <p>
+            {note.title}
+            {note.description}
+          </p>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/firebase.js
+++ src/firebase.js
@@ -9,6 +9,4 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APP_ID,
 };
 
-const app = initializeApp(firebaseConfig);
-
-export { app };
+export const app = initializeApp(firebaseConfig);
